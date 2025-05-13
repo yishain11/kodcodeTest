b279b9994b6e4efa4580a653b823cb548cbd0b91
yishain11 <yishain11@walla.co.il>
2025-05-13 09:15:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kodcodeTest
+{
+    internal class Program
+    {
+        static void greet() { }
+        static void Main(string[] args)
+        {
+            greet();
+        }
+    }
+}
